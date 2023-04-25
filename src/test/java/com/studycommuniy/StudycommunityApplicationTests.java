b6da4e1f3763af8de994e065123e65078b107d8f
SehyeonKang <gedfgrdt@naver.com>
2023-04-25 23:20:03 +0900

    project init, add dependency

---
+++ src/test/java/com/studycommuniy/StudycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studycommuniy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
