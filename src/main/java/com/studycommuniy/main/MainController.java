f7bd7061b414d4e48287851bd57676ae29ef9fc0
SehyeonKang <gedfgrdt@naver.com>
2023-05-09 22:50:00 +0900

    Feat: add @CurrentUser 인증된 사용자 여부를 확인하는 어노테이션 추가

---
+++ src/main/java/com/studycommuniy/main/MainController.java
@@ -0,0 +1,20 @@
+package com.studycommuniy.main;
+
+import com.studycommuniy.account.CurrentUser;
+import com.studycommuniy.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
