408fd686426152975fc592a28d48aa844c97f604
SehyeonKang <gedfgrdt@naver.com>
2023-06-15 22:15:22 +0900

    Feat: add homepage after login 로그인 이후 첫 페이지 구성 추가

--- src/main/java/com/studycommunity/modules/study/StudyRepositoryExtension.java
+++ src/main/java/com/studycommunity/modules/study/StudyRepositoryExtension.java
@@ -1,13 +1,18 @@
 package com.studycommunity.modules.study;
 
+import com.studycommunity.modules.tag.Tag;
+import com.studycommunity.modules.zone.Zone;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Transactional(readOnly = true)
 public interface StudyRepositoryExtension {
 
     Page<Study> findByKeyword(String keyword, Pageable pageable);
+
+    List<Study> findByAccount(Set<Tag> tags, Set<Zone> zones);
 }
